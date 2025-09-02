1324ce8f2b1a237ffacbbb88361014babcb6085a
Sumaiya-Meem <sumaiyameem.cse6.bu@gmail.com>
2025-09-02 16:31:56 +0600

    initalsetup done

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './index.css';
+
+function App() {
+  return (
+
+    <>
+      <div className="bg-purple-500">
+        <p className="text-3xl text-black-500 p-3">
+          Tailwind  is working
+        </p>
+    </div>
+    
+    </>
+  );
+}
+
+export default App;
